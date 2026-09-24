598190cad9cebc740d9f5d9962bbb69193ade45b
Realitaa <rezamuliaputra927@gmail.com>
2026-09-24 22:04:35 +0700

    feat: add contact page

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,14 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about', ['x' => random_int(1, 10)]);
 });
+
+Route::get('/contact', function () {
+    return view('contact', [
+        'data' => [
+            'name' => 'Reza Mulia Putra',
+            'class' => 'PSIK25B',
+            'nim' => 4251250010,
+            'discord' => 'realitaa'
+        ]
+    ]);
+});
